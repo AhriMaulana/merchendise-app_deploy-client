f1fb52aa0e4bb0ddd364159deda61011555b4aef
ahri <ahrimaulana97@gmail.com>
2022-05-30 17:04:37 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,16 +1,20 @@
-// import axios here ...
-import axios from 'axios'
 
-// Create base URL API
-export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1/",
-});
+export const API = () => {
+    const baseUrl =
+        process.env.REACT_APP_SERVER_URL ||
+        'https://merchendise-app.herokuapp.com/api/v1' ||
+        'https://localhost:5000/api/v1';
 
-// set auth token header
-export const setAuthToken = (token) => {
-    if (token) {
-        API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    } else {
-        delete API.defaults.headers.common["Authorization"];
-    }
+    const executeAPI = async (endpoint, config) => {
+        const response = await fetch(baseUrl + endpoint, config);
+        const data = await response.json();
+        return data;
+    };
+
+    return {
+        get: executeAPI,
+        post: executeAPI,
+        patch: executeAPI,
+        delete: executeAPI,
+    };
 };
